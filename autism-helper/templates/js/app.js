bb0790506668ab45999eb625a9e19a44cb5227bb
buibaohoang06 <heartbroken.89@protonmail.ch>
2021-11-27 22:42:14 +0700

    second commit, some bugs needed fix

---
+++ autism-helper/templates/js/app.js
@@ -0,0 +1,6 @@
+//default header
+const header = document.getElementById('import-header').innerHTML;
+
+//injecting head code
+console.log(header);
+document.getElementById('head').innerHTML = header;
