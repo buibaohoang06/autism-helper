d9990046c98bf3e2f677969be16f8486b00e122d
buibaohoang06 <heartbroken.89@protonmail.ch>
2021-11-28 21:51:21 +0700

    version 0.1.0 alpha

--- autism-helper/js/app.js
+++ autism-helper/js/app.js
@@ -1,3 +1,11 @@
+//audio playing
+
+let hungry = new Audio('audio/hungry.mp3');
+let play = new Audio('audio/play.mp3');
+let sleepy = new Audio('audio/sleepy.mp3');
+let toilet = new Audio('audio/toilet.mp3');
+let work = new Audio('audio/work.mp3');
+
 //default header
 const header = `
 <meta charset="utf-8">
@@ -23,18 +31,20 @@ const gettingInfo = `
 <div id="getting-info" class="center-align">
   <p>First of all, lets get to know you!</p>
   <div id="form">
-    <input id="name" type="text" placeholder="Enter your name."></input>
-    <input id="age" type="number" placeholder="Enter your age."></input>
+    <input id="name" type="text" placeholder="Enter your name.">
+    <input id="age" type="number" placeholder="Enter your age.">
+    <input id="email" type="email" placeholder="(For parents/guardian) Enter Email Address for Notifications">
     <button class="btn" id="submit" onclick="setData()">Submit</button>
     <script>
       let username = '';
       let age = '';
+      let email = '';
     </script>
   </div>
 </div>
 `
 
-//feeling
+
 
 //injecting head code
 document.getElementById('head').innerHTML = header;
@@ -45,12 +55,12 @@ document.getElementById('head').innerHTML = header;
 function setData(){
   username = document.getElementById('name').value;
   age = document.getElementById('age').value;
+  email = document.getElementById('email').value;
   window.localStorage.setItem('username', username);
   window.localStorage.setItem('age', age);
-  console.log(window.localStorage.getItem('username'));
-  console.log(window.localStorage.getItem('age'));
-  location.reload();
-};
+  window.localStorage.setItem('email', email);
+  location.reload()
+}
 //to app.html
 
 function toApp(){
@@ -60,18 +70,24 @@ function toApp(){
 //if user is feeling excelent
 function theActualApp(){
   document.getElementById('display').innerHTML = `
+    <script>
+    //tts
+    let speech = new SpeechSynthesisUtterance();
+    speech.lang = "en";
+    </script>
     <div id="the-actual-app">
       <p id="welcome" class="center-align"></p>
       <p class="center-align">What would you like to do now?</p>
       <div id="button-wrapper" class="center-align">
-        <button class="valign-wrapper padding-50px btn-flat waves-effect waves-grey center-align"><img src="images/healthy.png" class="center-align margin-auto middle" height="100" width="100"></button>
-        <button class="valign-wrapper padding-50px btn-flat waves-effect waves-grey center-align"><img src="images/sleeping.png" class="center-align margin-auto middle" height="100" width="100"></button>
-        <button class="valign-wrapper padding-50px btn-flat waves-effect waves-grey center-align"><img src="images/playtime.png" class="center-align margin-auto middle" height="100" width="100"></button>
-        <button class="valign-wrapper padding-50px btn-flat waves-effect waves-grey center-align"><img src="images/public-toilet.png" class="center-align margin-auto middle" height="100" width="100"></button>
-        <button class="valign-wrapper padding-50px btn-flat waves-effect waves-grey center-align"><img src="images/student.png" class="center-align margin-auto middle" height="100" width="100"></button>
+        <button class="valign-wrapper padding-50px btn-flat waves-effect waves-grey center-align" onclick="hungry.play()"><img src="images/healthy.png" class="center-align margin-auto middle" height="100" width="100"></button>
+        <button class="valign-wrapper padding-50px btn-flat waves-effect waves-grey center-align" onclick="sleepy.play()"><img src="images/sleeping.png" class="center-align margin-auto middle" height="100" width="100"></button>
+        <button class="valign-wrapper padding-50px btn-flat waves-effect waves-grey center-align" onclick="play.play()"><img src="images/playtime.png" class="center-align margin-auto middle" height="100" width="100"></button>
+        <button class="valign-wrapper padding-50px btn-flat waves-effect waves-grey center-align" onclick="toilet.play()"><img src="images/public-toilet.png" class="center-align margin-auto middle" height="100" width="100"></button>
+        <button class="valign-wrapper padding-50px btn-flat waves-effect waves-grey center-align" onclick="work.play()"><img src="images/student.png" class="center-align margin-auto middle" height="100" width="100"></button>
+        <input id="tts-request" class="center-align" placeholder="Can't find what you want to do here? Type here!" type="text"><button class="btn" onclick="var msg = new SpeechSynthesisUtterance(document.getElementById('tts-request').value); window.speechSynthesis.speak(msg);">Speak</button>
       </div>
       <div id="emergency" class="center-align">
-        <button class="btn waves-effect red darken-1">I am having an Emergency</button>
+        <button class="btn waves-effect red darken-1" onclick="emergency()">I am having an Emergency</button>
       </div>
     </div>
   `
@@ -83,4 +99,25 @@ if (localStorage.getItem('username') == undefined && localStorage.getItem('age')
 }
 else{
   theActualApp()
+}
+
+//Emergency
+
+function emergency(){
+  let sendingEmail = window.localStorage.getItem('email');
+  Email.send({
+    Host : "smtp.gmail.com",
+    Username : "hoang.nicolas2409@gmail.com",
+    Password : "vhsipkrewlcfiojg",
+    To : sendingEmail,
+    From : "hoang.nicolas2409@gmail.com",
+    Subject : "Emergency Signal from Autism Helper.",
+    Body : "User " + localStorage.getItem('username') + " is requesting emergency assistance!"
+}).then(
+  console.log('SOS Signal Sent!')
+);
+  var msg = new SpeechSynthesisUtterance('Help! I am in trouble!');
+  for (var i = 0; i < 10; i++){
+    window.speechSynthesis.speak(msg)
+  };
 }
